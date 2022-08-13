e586cd726281731bee63a7944f2c80ea35b30ecd
MartinKays <kubonamartinyafesi132@gmail.com>
2022-08-13 17:49:53 +0300

    Try the exit() in C functions

--- test2.c
+++ test2.c
@@ -1,9 +1,10 @@
 /* test2.c: This is my first C program */
 #include <stdio.h>
+#include <stdlib.h>
 
 main()
 {
     printf("My neighbour smokes weed! \n This is my first C Program. \n");
     int ch = 2;
-    return ch;
+    exit(1);
 }
