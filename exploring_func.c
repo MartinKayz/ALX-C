3da23931222825d768f5022d9a0257300bc7c036
MartinKays <kubonamartinyafesi132@gmail.com>
2022-08-15 20:38:22 +0300

    Exercise on functions

--- exploring_func.c
+++ exploring_func.c
@@ -8,12 +8,25 @@ int integer_add(int x, int y)
     int result;
     result = x + y;
     return result;
+
+}int interger_prod( int x, int y)
+{
+    int product;
+    product = x * y;
+    return product;
 }
+
+
 
 main()
 {
     int sum;
+    int mult;
     sum = integer_add(5,75);
     printf("The addition of 5 and 75 is %d. \n", sum);
+    mult = interger_prod(4,90);
+    printf("The multiplication of 4 and 90 is %d. \n", mult);
     return 0;
 }
+
+
